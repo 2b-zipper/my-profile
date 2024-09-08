42f2c899f67a187dba9fea55076504d2cd10a1cd
2B_enpitsu <pencil2b7777@gmail.com>
2024-09-08 14:48:39 +0900

    Initial commit

---
+++ source/main.cpp
@@ -0,0 +1,25 @@
+#include <3ds.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    gfxInitDefault();
+    consoleInit(GFX_TOP, NULL);
+
+	printf("Hi there ;)\n\nDiscord: 2ipper\nGitHub:  2b-zipper\nBluesky: 2ipper.bsky.social");
+
+	printf("\x1b[31;34HPress Start to exit.");
+
+    while(aptMainLoop()) {
+        gspWaitForVBlank();
+        hidScanInput();
+
+        if(hidKeysDown() & KEY_START)
+            break;
+
+        gfxFlushBuffers();
+        gfxSwapBuffers();
+    }
+
+    gfxExit();
+    return 0;
+}
